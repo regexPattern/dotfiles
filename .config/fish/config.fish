7cf71b050ab2ec5dc782f4c9f518c0c71fbca7f7
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2024-04-16 13:49:08 -0300

    refactor: cleaned a lot of the config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,31 @@
 set -gx fish_greeting
 
-set -gx EDITOR "nvim"
-set -gx FCEDIT "nvim"
-set -gx GIT_EDITOR "nvim"
-set -gx VISUAL "nvim"
+set -gx EDITOR "nvim --cmd 'let g:minimal = 1'"
+set -gx FCEDIT $EDITOR
+set -gx GIT_EDITOR $EDITOR
+set -gx VISUAL $EDITOR
 
 set -gx BAT_STYLE "plain"
+set -gx BAT_THEME "base16"
 
+set -gx FZF_DEFAULT_OPTS "--color=dark"
 set -gx FZF_DEFAULT_COMMAND "fd --type file --hidden"
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+
+set -gx HOMEBREW_NO_ENV_HINTS true
+
+set -gx nvm_default_version lts
 
 set -gx PNPM_HOME $HOME/Library/pnpm
 
 fish_add_path $HOME/.local/bin
-fish_add_path $HOME/Library/Python/**/bin
+fish_add_path $HOME/go/bin
 fish_add_path $PNPM_HOME
 fish_add_path /opt/homebrew/bin
 
 alias .. "cd .."
 alias ls "exa -la --group-directories-first"
 alias ls "exa -a --group-directories-first"
-alias cat "bat"
-
-# Returns not zero when MacOS's mode is not set to dark.
-if defaults read -globalDomain AppleInterfaceStyle &> /dev/null
-	source $__fish_config_dir/colors/github-dark-high-contrast.fish
-else
-	source $__fish_config_dir/colors/github-light-high-contrast.fish
-end
+alias cat bat
 
 zoxide init fish | source
