9feff736374eca69c8b667539c05f38412825838
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2026-03-10 23:05:43 -0300

    feat: agregada config de opencode, entre otras

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,16 @@
 alias ls 'eza --group-directories-first'
-alias ll 'eza -al --group-directories-first'
-alias tree 'eza -aT --git-ignore --group-directories-first'
+alias ll 'eza -al --group-directories-first --git-ignore'
+alias tree 'eza -aT --group-directories-first --git-ignore'
+alias oc 'opencode'
+alias lg 'lazygit'
 
 set --export --global EDITOR nvim
 set --export --global VISUAL $EDITOR
 
-fish_add_path ~/.local/bin
-fish_add_path ~/.local/share/bob/nvim-bin
+fish_add_path ~/.local/bin/
+fish_add_path ~/.local/share/bob/nvim-bin/
+fish_add_path ~/.amp/bin/
+fish_add_path ~/.opencode/bin/
 
 direnv hook fish | source
 fzf --fish | source
